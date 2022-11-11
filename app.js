1a8388ff3157779df1c4d09ccc412dd5616a66c6
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-11 20:13:54 +0000

    Creating different routes

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ const express = require("express");
 
 const app = express();
 
-console.log(app);
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+});
